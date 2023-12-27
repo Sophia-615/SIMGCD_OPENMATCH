407cc7d911a3e727029fc0d57855dcbc77d6435b
Sophia-615 <141799141+Sophia-615@users.noreply.github.com>
2023-12-27 21:46:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '/home/lxy/datasets/cifar10'
+cifar_100_root = '/home/lxy/datasets/cifar100'
+cub_root = '/home/lxy/datasets/cub'#已下载
+aircraft_root = '/home/lxy/datasets/fgvc-aircraft-2013b'#已下载
+car_root = '/home/lxy/datasets/cars'
+herbarium_dataroot = '/home/lxy/datasets/herbarium_19'
+imagenet_root = '/home/lxy/datasets/ImageNet'
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
